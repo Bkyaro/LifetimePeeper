61dad4c6833454dbe85d3c833576c58aaffa7ec1
Jackal_lzt <bkyarokun@gmail.com>
2023-12-12 14:03:59 +0800

    chore: modify loader styling

--- src/components/loader/index.tsx
+++ src/components/loader/index.tsx
@@ -1,19 +1,28 @@
+import React from "react";
 import "./styles.css";
 
 const Loader = () => {
-  return (
-    <div className="banterLoaderWrapper">
-      <div className="banterLoaderItem"></div>
-      <div className="banterLoaderItem"></div>
-      <div className="banterLoaderItem"></div>
-      <div className="banterLoaderItem"></div>
-      <div className="banterLoaderItem"></div>
-      <div className="banterLoaderItem"></div>
-      <div className="banterLoaderItem"></div>
-      <div className="banterLoaderItem"></div>
-      <div className="banterLoaderItem"></div>
-    </div>
-  );
+  const createBox = () => {
+    const boxes = [];
+
+    for (let i = 0; i < 9; i++) {
+      const randomColor = `rgb(${Math.floor(Math.random() * 255)},${Math.floor(
+        Math.random() * 255
+      )},${Math.floor(Math.random() * 255)})`;
+
+      const box = React.createElement("div", {
+        key: i,
+        className: "banterLoaderItem",
+        style: { backgroundColor: randomColor },
+      });
+
+      boxes.push(box);
+    }
+
+    return boxes;
+  };
+
+  return <div className="banterLoaderWrapper">{createBox()}</div>;
 };
 
 export default Loader;
